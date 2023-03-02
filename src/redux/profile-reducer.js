7a19d0aea8f53fa1e0765f897ffec1e388c03618
Nick Chay <nchaygta5@gmail.com>
2023-03-02 13:05:44 +0200

    lesson_48

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -12,22 +12,22 @@ let initialState = {
 const profileReducer = (state = initialState, action) => {
 
     switch (action.type) {
-        case UPDATE_NEW_POST_TEXT: {
-            let stateCopy = { ...state };
-            stateCopy.newPostText = action.newText;
-            return stateCopy;
-        };
+        case UPDATE_NEW_POST_TEXT:
+            return {
+                ...state,
+                newPostText: action.newText,
+            };
         case ADD_POST: {
             let newPost = {
                 id: 5,
                 message: state.newPostText,
                 likesCount: 0,
             };
-            let stateCopy = { ...state };
-            stateCopy.posts = [ ...state.posts ];
-            stateCopy.posts.push(newPost);
-            stateCopy.newPostText = '';
-            return stateCopy;
+            return {
+                ...state,
+                newPostText:'',
+                posts:[...state.posts ,newPost]
+            };
         };
         default:
             return state;
