2bb23579ebf5960ac1b54ac3b316f9370533fcf6
Nick Chay <nchaygta5@gmail.com>
2023-03-06 14:18:44 +0200

    lesson_54

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -4,14 +4,11 @@ import userPhoto from './../../assets/images/user.jpg'
 import React, { Component } from 'react'
 
 export class Users extends Component {
-    constructor(props) {
-        super(props);
-        
+    componentDidMount() {
         axios.get("https://social-network.samuraijs.com/api/1.0/users")
             .then(response => {
                 this.props.setUsers(response.data.items);
             });
-            debugger
     }
 
     render() {
